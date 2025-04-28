495933e42ead85c9548e7d1728c294eab77e6f25
George <gpm58@cornell.edu>
2025-04-28 16:30:46 -0400

    Test?

--- ECE5730_Smartwatch_SW.c
+++ ECE5730_Smartwatch_SW.c
@@ -1,6 +1,6 @@
 /**
  * Parker Schless (pis7), Katarina Duric (kd374), George Maidhof (gpm58)
- * 
+ *
  * Smartwatch Code
  *
  */
@@ -8,7 +8,10 @@
 #include "common.h"
 
 // Include custom libraries
-#include "pt_cornell_rp2040_v1_3.h"
+#include "pico/stdlib.h"
+#include "hardware/uart.h"
+#include "hardware/timer.h"
+// #include "pt_cornell_rp2040_v1_3.h"
 #include "font_ssh1106.h"
 #include "ssh1106.h"
 #include "i3g4250d.h"
@@ -19,11 +22,12 @@
 #include "dat/heartrate_img.h"
 #include "dat/activity_img.h"
 #include "dat/weather_img.h"
+#include "wifi_udp.h"
 
 // Some macros for max/min/abs
-#define min(a,b) ((a<b) ? a:b)
-#define max(a,b) ((a<b) ? b:a)
-#define abs(a) ((a>0) ? a:-a)
+#define min(a, b) ((a < b) ? a : b)
+#define max(a, b) ((a < b) ? b : a)
+#define abs(a) ((a > 0) ? a : -a)
 
 // GPIO Config
 #define CYCLE_BUTTON 14
@@ -40,7 +44,7 @@
 #define BATT_MAX 100
 
 // Direct Digital Synthesis (DDS) parameters for DAC
-#define two32 4294967296.0  // 2^32 (a constant)
+#define two32 4294967296.0 // 2^32 (a constant)
 #define Fs 50000
 #define DELAY 20 // 1/Fs (in microseconds)
 #define SOUND_DURATION 25000
@@ -48,19 +52,20 @@
 #define ALARM_IRQ TIMER_IRQ_0
 #define DAC_config_chan_A 0b0001000000000000
 #define sine_table_size 256
-volatile unsigned int STATE_0 = 0 ;
-volatile unsigned int count_0 = 0 ;
+volatile unsigned int STATE_0 = 0;
+volatile unsigned int count_0 = 0;
 volatile unsigned int phase_accum_main_0;
-volatile unsigned int phase_incr_main_0 = (400.0*two32)/Fs;
+volatile unsigned int phase_incr_main_0 = (400.0 * two32) / Fs;
 volatile unsigned int sound_curr_freq = 0;
-float current_amplitude_0 = 1.0 ;
+float current_amplitude_0 = 1.0;
 float sin_table[sine_table_size];
 int DAC_output_0;
 uint16_t DAC_data_0;
 int twinkle_twinkle[] = {261, 261, 392, 392, 440, 440, 392, 349, 349, 329, 329, 293, 293, 261};
 int tt_idx = 0;
 
-typedef enum {
+typedef enum
+{
   MM_TIME,
   MM_PHONE,
   MM_WEATHER,
@@ -70,7 +75,8 @@ typedef enum {
 } main_menu_state_t;
 main_menu_state_t main_menu_state = MM_TIME;
 
-typedef enum {
+typedef enum
+{
   DB_NOT_PRESSED,
   DB_MAYBE_PRESSED,
   DB_PRESSED,
@@ -80,102 +86,133 @@ debounce_state_t select_button_state = DB_NOT_PRESSED;
 debounce_state_t cycle_button_state = DB_NOT_PRESSED;
 int in_sub_menu = 0;
 
-static void alarm_irq(void) {
+static void alarm_irq(void)
+{
   hw_clear_bits(&timer_hw->intr, 1u << ALARM_NUM);
-  timer_hw->alarm[ALARM_NUM] = timer_hw->timerawl + DELAY ;
-  if (STATE_0 == 0) {
-    if (main_menu_state == MM_PHONE && in_sub_menu) {
+  timer_hw->alarm[ALARM_NUM] = timer_hw->timerawl + DELAY;
+  if (STATE_0 == 0)
+  {
+    if (main_menu_state == MM_PHONE && in_sub_menu)
+    {
       sound_curr_freq = twinkle_twinkle[tt_idx];
-      phase_incr_main_0 = (sound_curr_freq*two32)/Fs;
+      phase_incr_main_0 = (sound_curr_freq * two32) / Fs;
       phase_accum_main_0 += phase_incr_main_0;
       DAC_output_0 = (int)(current_amplitude_0 *
-        sin_table[phase_accum_main_0>>24]) + 2048;
+                           sin_table[phase_accum_main_0 >> 24]) +
+                     2048;
       DAC_data_0 = (DAC_config_chan_A | (DAC_output_0 & 0xffff));
-      spi_write16_blocking(SPI_PORT, &DAC_data_0, 1) ;
-      count_0 += 1 ;
-      if (count_0 == SOUND_DURATION) {
-        STATE_0 = 1 ;
-        count_0 = 0 ;
-        tt_idx = (tt_idx + 1) % (sizeof(twinkle_twinkle)/sizeof(twinkle_twinkle[0]));
+      spi_write16_blocking(SPI_PORT, &DAC_data_0, 1);
+      count_0 += 1;
+      if (count_0 == SOUND_DURATION)
+      {
+        STATE_0 = 1;
+        count_0 = 0;
+        tt_idx = (tt_idx + 1) % (sizeof(twinkle_twinkle) / sizeof(twinkle_twinkle[0]));
       }
     }
   }
-  else {
-    count_0 += 1 ;
-    if (count_0 == SOUND_DURATION) {
-      phase_accum_main_0 = 0 ;
-      STATE_0 = 0 ;
-      count_0 = 0 ;
+  else
+  {
+    count_0 += 1;
+    if (count_0 == SOUND_DURATION)
+    {
+      phase_accum_main_0 = 0;
+      STATE_0 = 0;
+      count_0 = 0;
     }
   }
 }
 
-void update_menu() {
+void update_menu()
+{
   int sel_pressed = gpio_get(SELECT_BUTTON);
-  switch (select_button_state) {
-    case DB_NOT_PRESSED:
-      if (sel_pressed) select_button_state = DB_MAYBE_PRESSED;
-      else select_button_state = DB_NOT_PRESSED;
-      break;
-    case DB_MAYBE_PRESSED:
-      if (sel_pressed) {
-        if (main_menu_state == MM_PHONE || main_menu_state == MM_WEATHER || main_menu_state == MM_ACTIVITY) {
-          in_sub_menu = 1;
-          SSH1106_Clear();
-        }
-        select_button_state = DB_PRESSED;
-      }
-      else select_button_state = DB_NOT_PRESSED;
-      break;
-    case DB_PRESSED:
-      if (sel_pressed) select_button_state = DB_PRESSED;
-      else select_button_state = DB_MAYBE_NOT_PRESSED;
-      break;
-    case DB_MAYBE_NOT_PRESSED:
-      if (sel_pressed) select_button_state = DB_PRESSED;
-      else select_button_state = DB_NOT_PRESSED;
-      break;
-    default:
+  switch (select_button_state)
+  {
+  case DB_NOT_PRESSED:
+    if (sel_pressed)
+      select_button_state = DB_MAYBE_PRESSED;
+    else
       select_button_state = DB_NOT_PRESSED;
-      break;
+    break;
+  case DB_MAYBE_PRESSED:
+    if (sel_pressed)
+    {
+      if (main_menu_state == MM_PHONE || main_menu_state == MM_WEATHER || main_menu_state == MM_ACTIVITY)
+      {
+        in_sub_menu = 1;
+        SSH1106_Clear();
+      }
+      select_button_state = DB_PRESSED;
+    }
+    else
+      select_button_state = DB_NOT_PRESSED;
+    break;
+  case DB_PRESSED:
+    if (sel_pressed)
+      select_button_state = DB_PRESSED;
+    else
+      select_button_state = DB_MAYBE_NOT_PRESSED;
+    break;
+  case DB_MAYBE_NOT_PRESSED:
+    if (sel_pressed)
+      select_button_state = DB_PRESSED;
+    else
+      select_button_state = DB_NOT_PRESSED;
+    break;
+  default:
+    select_button_state = DB_NOT_PRESSED;
+    break;
   }
 
   int cyc_pressed = gpio_get(CYCLE_BUTTON);
-  switch (cycle_button_state) {
-    case DB_NOT_PRESSED:
-      if (cyc_pressed) cycle_button_state = DB_MAYBE_PRESSED;
-      else cycle_button_state = DB_NOT_PRESSED;
-      break;
-    case DB_MAYBE_PRESSED:
-      if (cyc_pressed) {
-        if (in_sub_menu) in_sub_menu = 0;
-        else main_menu_state = (main_menu_state == MM_BATTERY) ? MM_TIME : (main_menu_state + 1);
-        SSH1106_Clear();
-        cycle_button_state = DB_PRESSED;
-      }
-      else cycle_button_state = DB_NOT_PRESSED;
-      break;
-    case DB_PRESSED:
-      if (cyc_pressed) cycle_button_state = DB_PRESSED;
-      else cycle_button_state = DB_MAYBE_NOT_PRESSED;
-      break;
-    case DB_MAYBE_NOT_PRESSED:
-      if (cyc_pressed) cycle_button_state = DB_PRESSED;
-      else cycle_button_state = DB_NOT_PRESSED;
-      break;
-    default:
+  switch (cycle_button_state)
+  {
+  case DB_NOT_PRESSED:
+    if (cyc_pressed)
+      cycle_button_state = DB_MAYBE_PRESSED;
+    else
       cycle_button_state = DB_NOT_PRESSED;
-      break;
+    break;
+  case DB_MAYBE_PRESSED:
+    if (cyc_pressed)
+    {
+      if (in_sub_menu)
+        in_sub_menu = 0;
+      else
+        main_menu_state = (main_menu_state == MM_BATTERY) ? MM_TIME : (main_menu_state + 1);
+      SSH1106_Clear();
+      cycle_button_state = DB_PRESSED;
+    }
+    else
+      cycle_button_state = DB_NOT_PRESSED;
+    break;
+  case DB_PRESSED:
+    if (cyc_pressed)
+      cycle_button_state = DB_PRESSED;
+    else
+      cycle_button_state = DB_MAYBE_NOT_PRESSED;
+    break;
+  case DB_MAYBE_NOT_PRESSED:
+    if (cyc_pressed)
+      cycle_button_state = DB_PRESSED;
+    else
+      cycle_button_state = DB_NOT_PRESSED;
+    break;
+  default:
+    cycle_button_state = DB_NOT_PRESSED;
+    break;
   }
 }
 
-int map_batt(int input, int input_min, int input_max, int output_min, int output_max) {
+int map_batt(int input, int input_min, int input_max, int output_min, int output_max)
+{
   int output = (int)((float)(input - input_min) / (float)(input_max - input_min) * (float)(output_max - output_min) + (float)output_min);
   output = ((output + 5) / 10) * 10;
   return output;
 }
 
-int main() {
+int main()
+{
 
   // Initialize standard I/O
   stdio_init_all();
@@ -235,8 +272,9 @@ int main() {
   // Build the sine lookup table
   // scaled to produce values between 0 and 4096 (for 12-bit DAC)
   int ii;
-  for (ii = 0; ii < sine_table_size; ii++) {
-    sin_table[ii] = 2047.0*sin((float)ii*6.283/(float)sine_table_size);
+  for (ii = 0; ii < sine_table_size; ii++)
+  {
+    sin_table[ii] = 2047.0 * sin((float)ii * 6.283 / (float)sine_table_size);
   }
 
   // Enable the interrupt for the alarm (we're using Alarm 0)
@@ -248,81 +286,97 @@ int main() {
   // Write the lower 32 bits of the target time to the alarm register, arming it.
   timer_hw->alarm[ALARM_NUM] = timer_hw->timerawl + DELAY;
 
-  while (true) {
+  // Initialize the UDP server
+  wifi_udp_init();
+  start_wifi_threads();
+
+  while (true)
+  {
 
     update_menu();
-    switch (main_menu_state) {
-      case MM_TIME:
-        sprintf(prev_time_str, "%s", time_str);
-        uptime_ms = time_us_64() / 1000;
-        hours = (uptime_ms / (1000 * 60 * 60)) % 24;
-        minutes = (uptime_ms / (1000 * 60)) % 60;
-        seconds = (uptime_ms / 1000) % 60;
-        sprintf(time_str, "%02u:%02u:%02u", hours, minutes, seconds);
-        if (seconds != prev_seconds) {
-          prev_seconds = seconds;
-          SSH1106_GotoXY(25, 25);
-          SSH1106_Puts(prev_time_str, &Font_11x18, 0);
-          SSH1106_GotoXY(25, 25);
-          SSH1106_Puts(time_str, &Font_11x18, 1);
-          SSH1106_UpdateScreen();
-        }
-        break;
-      case MM_PHONE:
-        if (in_sub_menu) {
-          sprintf(phone_str, "Song!");
-          SSH1106_GotoXY(40, 25);
-          SSH1106_Puts(phone_str, &Font_11x18, 1);
-        } else {
-          for (int i = 0; i < sizeof(phone_img)/sizeof(phone_img[0]); i++)
-            SSH1106_DrawPixel(phone_img[i][0], phone_img[i][1], 1);
-        }
+    switch (main_menu_state)
+    {
+    case MM_TIME:
+      sprintf(prev_time_str, "%s", time_str);
+      uptime_ms = time_us_64() / 1000;
+      hours = (uptime_ms / (1000 * 60 * 60)) % 24;
+      minutes = (uptime_ms / (1000 * 60)) % 60;
+      seconds = (uptime_ms / 1000) % 60;
+      sprintf(time_str, "%02u:%02u:%02u", hours, minutes, seconds);
+      if (seconds != prev_seconds)
+      {
+        prev_seconds = seconds;
+        SSH1106_GotoXY(25, 25);
+        SSH1106_Puts(prev_time_str, &Font_11x18, 0);
+        SSH1106_GotoXY(25, 25);
+        SSH1106_Puts(time_str, &Font_11x18, 1);
         SSH1106_UpdateScreen();
-        break;
-      case MM_WEATHER:
-        if (in_sub_menu) {
-          sprintf(weather_str, "Temp: %dC", read_temp());
-          SSH1106_GotoXY(10, 25);
-          SSH1106_Puts(weather_str, &Font_11x18, 1);
-        } else {
-          for (int i = 0; i < sizeof(weather_img)/sizeof(weather_img[0]); i++)
-            SSH1106_DrawPixel(weather_img[i][0], weather_img[i][1], 1);
-        }
-        SSH1106_UpdateScreen();
-        break;
-      case MM_HEART_RATE:
-        for (int i = 0; i < sizeof(heartrate_img)/sizeof(heartrate_img[0]); i++)
-          SSH1106_DrawPixel(heartrate_img[i][0], heartrate_img[i][1], 1);
-        SSH1106_UpdateScreen();
-        break;
-      case MM_ACTIVITY:
-        if (in_sub_menu) {
-          for (int i = 0; i < 128; i++)
-            for (int j = 0; j < 64; j++)
-              SSH1106_DrawPixel(i, j, SSH1106_COLOR_BLACK);
-          sprintf(gyro_x_str, "X: %d", read_gyro_x());
-          sprintf(gyro_y_str, "Y: %d", read_gyro_y());
-          sprintf(gyro_z_str, "Z: %d", read_gyro_z());
-          SSH1106_GotoXY(10, 6);
-          SSH1106_Puts(gyro_x_str, &Font_11x18, 1);
-          SSH1106_GotoXY(10, 25);
-          SSH1106_Puts(gyro_y_str, &Font_11x18, 1);
-          SSH1106_GotoXY(10, 44);
-          SSH1106_Puts(gyro_z_str, &Font_11x18, 1);
-        } else {
-          for (int i = 0; i < sizeof(activity_img)/sizeof(activity_img[0]); i++)
-            SSH1106_DrawPixel(activity_img[i][0], activity_img[i][1], 1);
-        }
-        SSH1106_UpdateScreen();   
-        break;                                                                                 
-      case MM_BATTERY:
-        for (int i = 0; i < sizeof(battery_img)/sizeof(battery_img[0]); i++)
-          SSH1106_DrawPixel(battery_img[i][0], battery_img[i][1], 1);
-        sprintf(battery_str, "%02d%%", map_batt(adc_read(), ADC_MIN, ADC_MAX, BATT_MIN, BATT_MAX));
-        SSH1106_GotoXY(50, 40);
-        SSH1106_Puts(battery_str, &Font_11x18, 1);
-        SSH1106_UpdateScreen();
-        break;
+      }
+      break;
+    case MM_PHONE:
+      if (in_sub_menu)
+      {
+        sprintf(phone_str, "Song!");
+        SSH1106_GotoXY(40, 25);
+        SSH1106_Puts(phone_str, &Font_11x18, 1);
+      }
+      else
+      {
+        for (int i = 0; i < sizeof(phone_img) / sizeof(phone_img[0]); i++)
+          SSH1106_DrawPixel(phone_img[i][0], phone_img[i][1], 1);
+      }
+      SSH1106_UpdateScreen();
+      break;
+    case MM_WEATHER:
+      if (in_sub_menu)
+      {
+        sprintf(weather_str, "Temp: %dC", read_temp());
+        SSH1106_GotoXY(10, 25);
+        SSH1106_Puts(weather_str, &Font_11x18, 1);
+      }
+      else
+      {
+        for (int i = 0; i < sizeof(weather_img) / sizeof(weather_img[0]); i++)
+          SSH1106_DrawPixel(weather_img[i][0], weather_img[i][1], 1);
+      }
+      SSH1106_UpdateScreen();
+      break;
+    case MM_HEART_RATE:
+      for (int i = 0; i < sizeof(heartrate_img) / sizeof(heartrate_img[0]); i++)
+        SSH1106_DrawPixel(heartrate_img[i][0], heartrate_img[i][1], 1);
+      SSH1106_UpdateScreen();
+      break;
+    case MM_ACTIVITY:
+      if (in_sub_menu)
+      {
+        for (int i = 0; i < 128; i++)
+          for (int j = 0; j < 64; j++)
+            SSH1106_DrawPixel(i, j, SSH1106_COLOR_BLACK);
+        sprintf(gyro_x_str, "X: %d", read_gyro_x());
+        sprintf(gyro_y_str, "Y: %d", read_gyro_y());
+        sprintf(gyro_z_str, "Z: %d", read_gyro_z());
+        SSH1106_GotoXY(10, 6);
+        SSH1106_Puts(gyro_x_str, &Font_11x18, 1);
+        SSH1106_GotoXY(10, 25);
+        SSH1106_Puts(gyro_y_str, &Font_11x18, 1);
+        SSH1106_GotoXY(10, 44);
+        SSH1106_Puts(gyro_z_str, &Font_11x18, 1);
+      }
+      else
+      {
+        for (int i = 0; i < sizeof(activity_img) / sizeof(activity_img[0]); i++)
+          SSH1106_DrawPixel(activity_img[i][0], activity_img[i][1], 1);
+      }
+      SSH1106_UpdateScreen();
+      break;
+    case MM_BATTERY:
+      for (int i = 0; i < sizeof(battery_img) / sizeof(battery_img[0]); i++)
+        SSH1106_DrawPixel(battery_img[i][0], battery_img[i][1], 1);
+      sprintf(battery_str, "%02d%%", map_batt(adc_read(), ADC_MIN, ADC_MAX, BATT_MIN, BATT_MAX));
+      SSH1106_GotoXY(50, 40);
+      SSH1106_Puts(battery_str, &Font_11x18, 1);
+      SSH1106_UpdateScreen();
+      break;
     }
   }
 }
---
+++ wifi_udp.c
@@ -0,0 +1,193 @@
+// wifi_udp.c
+
+// Standard libraries
+#include <string.h>
+
+// LWIP libraries
+#include "lwip/pbuf.h"
+#include "lwip/udp.h"
+
+// Pico SDK hardware support libraries
+#include "hardware/sync.h"
+
+// Protothreads
+#include "pico/stdlib.h"
+#include "hardware/uart.h"
+#include "hardware/timer.h"
+#include "pt_cornell_rp2040_v1_3.h"
+
+// Our header for making WiFi connection
+#include "connect.h"
+#include "wifi_udp.h"
+
+// WiFi credentials
+#include "pico/cyw43_arch.h"
+#include "lwip/netif.h"
+
+// Destination port and IP address
+#define UDP_PORT 1234
+#define BEACON_TARGET "172.20.10.2"
+
+// Maximum length of our message
+#define BEACON_MSG_LEN_MAX 127
+
+// =======================
+// Global Variables
+
+// Protocol control block for UDP receive connection
+static struct udp_pcb *udp_rx_pcb;
+
+// Buffer in which to copy received messages
+char received_data[BEACON_MSG_LEN_MAX];
+
+// Semaphore for signaling a new received message
+struct pt_sem new_message;
+
+// WiFi MAC address storage
+char mac_address_str[18];
+
+// =======================
+// Internal Helper Functions
+
+static void udpecho_raw_recv(void *arg, struct udp_pcb *upcb, struct pbuf *p,
+                             const ip_addr_t *addr, u16_t port)
+{
+  LWIP_UNUSED_ARG(arg);
+
+  if (p != NULL)
+  {
+    memcpy(received_data, p->payload, BEACON_MSG_LEN_MAX);
+    PT_SEM_SAFE_SIGNAL(pt, &new_message);
+    memset(p->payload, 0, BEACON_MSG_LEN_MAX + 1);
+    pbuf_free(p);
+  }
+  else
+  {
+    printf("NULL pbuf in callback\n");
+  }
+}
+
+static void udpecho_raw_init(void)
+{
+  udp_rx_pcb = udp_new_ip_type(IPADDR_TYPE_ANY);
+
+  if (udp_rx_pcb != NULL)
+  {
+    err_t err = udp_bind(udp_rx_pcb, netif_ip_addr4(netif_default), UDP_PORT);
+    if (err == ERR_OK)
+    {
+      udp_recv(udp_rx_pcb, udpecho_raw_recv, NULL);
+    }
+    else
+    {
+      printf("Bind error!\n");
+    }
+  }
+  else
+  {
+    printf("UDP PCB allocation error!\n");
+  }
+}
+
+static void get_MAC_address(char *mac)
+{
+  struct netif *netif = netif_default;
+  if (netif == NULL)
+  {
+    sprintf(mac, "00:00:00:00:00:00");
+    return;
+  }
+
+  const uint8_t *mac_addr = (const uint8_t *)netif->hwaddr;
+  sprintf(mac, "%02X:%02X:%02X:%02X:%02X:%02X",
+          mac_addr[0], mac_addr[1], mac_addr[2],
+          mac_addr[3], mac_addr[4], mac_addr[5]);
+}
+
+// =======================
+// Protothreads for Send and Receive
+
+static PT_THREAD(protothread_receive(struct pt *pt))
+{
+  PT_BEGIN(pt);
+
+  while (1)
+  {
+    PT_SEM_SAFE_WAIT(pt, &new_message);
+
+    // Print received message
+    printf("Received: %s\n", received_data);
+
+    // You can parse received command here if needed
+  }
+
+  PT_END(pt);
+}
+
+static PT_THREAD(protothread_send(struct pt *pt))
+{
+  PT_BEGIN(pt);
+
+  static struct udp_pcb *pcb;
+  pcb = udp_new();
+
+  static ip_addr_t addr;
+  ipaddr_aton(BEACON_TARGET, &addr);
+
+  while (1)
+  {
+    // Prompt the user
+    sprintf(pt_serial_out_buffer, "> ");
+    serial_write;
+
+    // Perform a non-blocking serial read for a string
+    serial_read;
+
+    // Allocate a PBUF
+    struct pbuf *p = pbuf_alloc(PBUF_TRANSPORT, BEACON_MSG_LEN_MAX + 1, PBUF_RAM);
+
+    char *req = (char *)p->payload;
+    memset(req, 0, BEACON_MSG_LEN_MAX + 1);
+
+    // Fill the pbuf payload
+    snprintf(req, BEACON_MSG_LEN_MAX, "%s: %s\n",
+             ip4addr_ntoa(netif_ip_addr4(netif_default)), pt_serial_in_buffer);
+
+    // Send the UDP packet
+    err_t er = udp_sendto(pcb, p, &addr, UDP_PORT);
+
+    pbuf_free(p);
+
+    if (er != ERR_OK)
+    {
+      printf("Failed to send UDP packet! error=%d\n", er);
+    }
+  }
+
+  PT_END(pt);
+}
+
+// =======================
+// Public Functions (called by main app)
+
+void wifi_udp_init()
+{
+  if (connectWifi(country, WIFI_SSID, WIFI_PASSWORD, auth))
+  {
+    printf("Failed WiFi connection.\n");
+    return;
+  }
+
+  get_MAC_address(mac_address_str);
+  printf("Pico W MAC Address: %s\n", mac_address_str);
+
+  PT_SEM_INIT(&new_message, 0);
+
+  udpecho_raw_init();
+}
+
+void start_wifi_threads()
+{
+  pt_add_thread(protothread_send);
+  pt_add_thread(protothread_receive);
+}
